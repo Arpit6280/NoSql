7480cc6a918f7f8d36591e7c3bff67f7ae15981c
Arpit Singh Yadav <arpitsinghyadav19@gmail.com>
2024-05-05 13:02:12 +0530

    deleting cart items

--- models/user.js
+++ models/user.js
@@ -19,18 +19,65 @@ class User {
       .catch((err) => console.log(err));
   }
   addToCart(product) {
-    // const cartProduct = this.cart.items.findIndex((cp) => {
-    //   return cp._id === product._id;
-    // });
-    // product.quantity
+    console.log(product);
+    const cartProductIndex = this.cart.items.findIndex((cp) => {
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: newQuantity,
+      });
+    }
+
     const updatedCart = {
-      items: [{ productId: new ObjectId(product._id), quantity: 1 }],
+      items: updatedCartItems,
     };
     const db = getDb();
     return db.collection("users").updateOne(
       { _id: new ObjectId(this._id) },
       {
         $set: { cart: updatedCart },
+      }
+    );
+  }
+
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map((i) => {
+      return i.productId;
+    });
+    return db
+      .collection("products")
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then((products) => {
+        return products.map((p) => {
+          return {
+            ...p,
+            quantity: this.cart.items.find((i) => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity,
+          };
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  deleteCartItem(prodId) {
+    const updatedCartItems = this.cart.items.filter((item) => {
+      return item.productId.toString() !== prodId.toString();
+    });
+    const db = getDb();
+    return db.collection("users").updateOne(
+      { _id: new ObjectId(this._id) },
+      {
+        $set: { cart: { items: updatedCartItems } },
       }
     );
   }
